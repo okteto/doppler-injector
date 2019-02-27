f18c9f06dbba411797d965966102b9c5c1a6c622
Ramiro Berrelleza <ramiro@okteto.com>
2019-02-26 18:44:20 -0800

    catch exception and see if file exists

--- app/index.js
+++ app/index.js
@@ -3,12 +3,23 @@ const readline = require('readline');
 const backup = '/var/secret/doppler/.env'
 const result = '/var/secret/doppler/api.env'
 
-require("doppler-client")({
-    api_key: process.env.DOPPLER_API_KEY,
-    pipeline: process.env.DOPPLER_PIPELINE,
-    environment: process.env.DOPPLER_ENVIRONMENT,
-    backup_filepath: backup
-  })
+try{
+  require("doppler-client")({
+      api_key: process.env.DOPPLER_API_KEY,
+      pipeline: process.env.DOPPLER_PIPELINE,
+      environment: process.env.DOPPLER_ENVIRONMENT,
+      backup_filepath: backup
+    })
+} catch (e) {
+  console.error(`doppler api call failed, will use cache if available: ${e}`)
+}
+
+fs.access(backup, fs.F_OK, (err) => {
+  if (err) {
+    console.error(`${backup} doesn't exist`)
+    process.exit(1)
+  }
+})
 
 const readFile = readline.createInterface({
     input: fs.createReadStream(backup),
@@ -23,4 +34,4 @@ readFile
 function transform(line) {
     var arr = line.split("=").map(val => val.trim())
     this.output.write(`export ${arr[0]}=${arr[1]}\n`);
-  }
+}
